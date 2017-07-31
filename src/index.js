6d686e71aeb8486822f43f83b6adc5fd88198b4c
Bohao Chen <bohaoc@curacloudcorp.com>
2017-07-31 19:59:11 +0800

    Support color placeholder

--- src/index.js
+++ src/index.js
@@ -8,14 +8,25 @@ $().ready(() => {
 
   const appendToDOM = (id) => {
     $('#container').append(`<div class="card" id="card-${id}"></div>`);
+    const width = $('#card-0').width();
     const imgSrc = _data[id].posterURL;
     const image = $('<img />');
+    const placeholder = $('<div />')
+      .css('background-color', _data[id].color || '#ffffff')
+      .css('width', width)
+      .css('height', (_data[id].h / _data[id].w) * width);
+    $(`#card-${id}`).append(placeholder);
     image.attr('width', '100%').attr('src', imgSrc)
       .css('opacity', 0).css('display', 'block')
       .on('load', () => {
         image.animate({
           opacity: 1,
         }, 500);
+        placeholder.animate({
+          opacity: 0,
+        }, 500, 'swing', () => {
+          placeholder.remove();
+        });
       });
     $(`#card-${id}`).append(image);
     if (id === totalLen) isEnd = true;
